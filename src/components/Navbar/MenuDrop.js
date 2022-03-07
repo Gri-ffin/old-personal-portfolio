a34ae48e10e1742be60a8d3896e3953731194174
Gri-ffin <gameshunter359@gmail.com>
2022-03-07 19:36:58 +0100

    Create the MenuDrop component

---
+++ src/components/Navbar/MenuDrop.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  useColorModeValue,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Link
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
+
+export function MenuDrop({ IconButton, ReactLink }) {
+  return (
+    <Menu>
+      <MenuButton
+        as={IconButton}
+        aria-label="Options"
+        icon={<HamburgerIcon />}
+        variant="outline"
+        bg={useColorModeValue('whiteAlpha.900', 'gray.700')}
+      />
+      <MenuList bg={useColorModeValue('gray.800', 'whiteAlpha.900')}>
+        <Link
+          as={ReactLink}
+          to="/"
+          color={useColorModeValue('whiteAlpha.900', 'gray.800')}
+        >
+          <MenuItem _focus={'none'}>Home</MenuItem>
+        </Link>
+        <Link
+          as={ReactLink}
+          to="/about"
+          color={useColorModeValue('whiteAlpha.900', 'gray.800')}
+        >
+          <MenuItem _focus={'none'}>About</MenuItem>
+        </Link>
+        <Link
+          as={ReactLink}
+          to="/works"
+          color={useColorModeValue('whiteAlpha.900', 'gray.800')}
+        >
+          <MenuItem _focus={'none'}>Projects</MenuItem>
+        </Link>
+        <Link
+          as="a"
+          href="https://github.com/Gri-ffin/personal-portfolio"
+          color={useColorModeValue('whiteAlpha.900', 'gray.800')}
+          isExternal
+        >
+          <MenuItem _focus="none">Code source</MenuItem>
+        </Link>
+      </MenuList>
+    </Menu>
+  )
+}
