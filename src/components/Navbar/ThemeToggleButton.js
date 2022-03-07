89fae50f0ce2d69fc3658ca3033dc1ff39b6ac8f
Gri-ffin <gameshunter359@gmail.com>
2022-03-07 20:23:05 +0100

    Add theme toggle button for light/dark mode

---
+++ src/components/Navbar/ThemeToggleButton.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { useColorMode, IconButton, useColorModeValue } from '@chakra-ui/react'
+import { SunIcon, MoonIcon } from '@chakra-ui/icons'
+
+const ThemeToggleButton = () => {
+  const { toggleColorMode } = useColorMode()
+
+  return (
+    <IconButton
+      aria-label="Toggle theme"
+      colorScheme={useColorModeValue('purple', 'orange')}
+      icon={useColorModeValue(<MoonIcon />, <SunIcon />)}
+      onClick={toggleColorMode}
+    ></IconButton>
+  )
+}
+
+export default ThemeToggleButton
