aa29200f4cbde74e88a59bfaf6049970dc171222
Gri-ffin <gameshunter359@gmail.com>
2022-03-08 18:23:20 +0100

    Add typing animation to home page | closes #2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Box, useColorModeValue } from '@chakra-ui/react'
+import { Box, Container, Heading, useColorModeValue } from '@chakra-ui/react'
 import ParticleBackground from '../components/Particles/Index'
+import Typewriter from 'typewriter-effect'
 
 const HomePage = () => {
   return (
@@ -8,9 +9,29 @@ const HomePage = () => {
       w="100vw"
       h="100vh"
       bg={useColorModeValue('gray.500', 'gray.800')}
-      color="white"
+      color={useColorModeValue('gray.800', 'whiteAlpha.900')}
     >
       <ParticleBackground />
+      <Container
+        w="100%"
+        h="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Heading as="h1" align="center">
+          Hello, I&apos;m Yassine Tadlaoui, I&apos;m a
+          <Typewriter
+            options={{
+              strings: ['Developer.', 'Code enthusiast.', 'Gamer.'],
+              autoStart: true,
+              delay: 'natural',
+              deleteSpeed: 'natural',
+              loop: true
+            }}
+          />
+        </Heading>
+      </Container>
     </Box>
   )
 }
