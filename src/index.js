3f6b1568ba5922fe9b6f5561a7ced471d3a5c9bd
Gri-ffin <gameshunter359@gmail.com>
2022-03-09 19:04:36 +0100

    Add fonts

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
+import Fonts from './lib/Fonts'
 import theme from './lib/theme'
 
 ReactDOM.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <BrowserRouter>
+        <Fonts />
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         <App />
       </BrowserRouter>
---
+++ src/lib/Fonts.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Global } from '@emotion/react'
+
+const Fonts = () => {
+  return (
+    <Global
+      styles={`
+    @import url('https://fonts.googleapis.com/css2?family=M+PLUS+Rounded+1c:wght@300;700&display=swap');
+    `}
+    />
+  )
+}
+
+export default Fonts
