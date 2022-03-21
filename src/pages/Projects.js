aef7a72020ad714d2aa1b28aa9252f78326b2584
Gri-ffin <gameshunter359@gmail.com>
2022-03-21 22:24:41 +0100

    add projects page

---
+++ src/pages/Projects.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Container, Heading } from '@chakra-ui/react'
+import { Section } from '../components/Section'
+import { ProjectGridItem } from '../components/Projects/ProjectGridItem'
+import discordCloneImage from '../images/discord-public-chat.png'
+import spotifyCloneImage from '../images/spotifyClone.png'
+
+const ProjectsPage = () => {
+  return (
+    <>
+      <Container pt={28}>
+        <Heading as="h2" fontSize={20} mb={6}>
+          My Projects
+        </Heading>
+
+        <Container>
+          <Section>
+            <ProjectGridItem
+              id="discord-clone"
+              title="Discord/public-chat"
+              image={discordCloneImage}
+            >
+              A simple personal project with a discord front end where you can
+              connect with your google account and chat in a public rooms with
+              other people.
+            </ProjectGridItem>
+          </Section>
+        </Container>
+        <Container>
+          <Section>
+            <ProjectGridItem
+              id="spotify-clone"
+              title="Spotify Clone"
+              image={spotifyCloneImage}
+            >
+              A school project where I created a lightweight spotify clone where
+              I can connect with my spotify account and listen to my favorite
+              music.
+            </ProjectGridItem>
+          </Section>
+        </Container>
+      </Container>
+    </>
+  )
+}
+export default ProjectsPage
