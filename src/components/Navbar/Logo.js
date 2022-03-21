05d58bab252a261abacaf427ff8d7e02a1dc9aa8
Gri-ffin <gameshunter359@gmail.com>
2022-03-21 21:47:11 +0100

    fix animation

--- src/components/Navbar/Logo.js
+++ src/components/Navbar/Logo.js
@@ -13,40 +13,17 @@ const LogoBox = styled.span`
   line-height: 20px;
   padding: 10px;
 
-  @-webkit-keyframes img /* Safari and Chrome */ {
+  @-webkit-keyframes img {
     from {
       -webkit-transform: rotate(0deg);
-      -o-transform: rotate(0deg);
-      transform: rotate(0deg);
     }
     to {
       -webkit-transform: rotate(360deg);
-      -o-transform: rotate(360deg);
-      transform: rotate(360deg);
     }
   }
-  @keyframes img {
-    from {
-      -ms-transform: rotate(0deg);
-      -moz-transform: rotate(0deg);
-      -webkit-transform: rotate(0deg);
-      -o-transform: rotate(0deg);
-      transform: rotate(0deg);
-    }
-    to {
-      -ms-transform: rotate(360deg);
-      -moz-transform: rotate(360deg);
-      -webkit-transform: rotate(360deg);
-      -o-transform: rotate(360deg);
-      transform: rotate(360deg);
-    }
-  }
+
   img {
     -webkit-animation: rotating 2s linear infinite;
-    -moz-animation: rotating 2s linear infinite;
-    -ms-animation: rotating 2s linear infinite;
-    -o-animation: rotating 2s linear infinite;
-    animation: rotating 2s linear infinite;
   }
 `
 
