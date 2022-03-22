6dfa24d3e78c6d78ae22ea7cd686379bbba2d98a
Gri-ffin <gameshunter359@gmail.com>
2022-03-22 20:51:36 +0100

    improvments

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,43 +1,51 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Container, Heading } from '@chakra-ui/react'
 import { Section } from '../components/Section'
 import { ProjectGridItem } from '../components/Projects/Projects'
-import discordCloneImage from '../images/projects/discord-clone.png'
-import spotifyCloneImage from '../images/projects/spotify-clone.png'
+import LoadingSpinner from '../components/LoadingSpinner'
+const discordCloneImage = React.lazy(
+  import('../images/projects/discord-clone.png')
+)
+const spotifyCloneImage = React.lazy(
+  import('../images/projects/spotify-clone.png')
+)
 
 const ProjectsPage = () => {
   return (
-    <Container pt={28}>
-      <Heading as="h2" fontSize={20} mb={6}>
-        My Projects
-      </Heading>
+    <Suspense fallback={<LoadingSpinner />}>
+      <Container pt={28}>
+        <Heading as="h2" fontSize={20} mb={6}>
+          My Projects
+        </Heading>
 
-      <Container>
-        <Section>
-          <ProjectGridItem
-            id="discord-clone"
-            title="Discord/public-chat"
-            image={discordCloneImage}
-          >
-            A simple personal project with a discord front end where you can
-            connect with your google account and chat in a public rooms with
-            other people.
-          </ProjectGridItem>
-        </Section>
+        <Container>
+          <Section>
+            <ProjectGridItem
+              id="discord-clone"
+              title="Discord/public-chat"
+              image={discordCloneImage}
+            >
+              A simple personal project with a discord front end where you can
+              connect with your google account and chat in a public rooms with
+              other people.
+            </ProjectGridItem>
+          </Section>
+        </Container>
+        <Container>
+          <Section>
+            <ProjectGridItem
+              id="spotify-clone"
+              title="Spotify Clone"
+              image={spotifyCloneImage}
+            >
+              A school project where I created a lightweight spotify clone where
+              I can connect with my spotify account and listen to my favorite
+              music.
+            </ProjectGridItem>
+          </Section>
+        </Container>
       </Container>
-      <Container>
-        <Section>
-          <ProjectGridItem
-            id="spotify-clone"
-            title="Spotify Clone"
-            image={spotifyCloneImage}
-          >
-            A school project where I created a lightweight spotify clone where I
-            can connect with my spotify account and listen to my favorite music.
-          </ProjectGridItem>
-        </Section>
-      </Container>
-    </Container>
+    </Suspense>
   )
 }
 export default ProjectsPage
