3bb65e74cddc56c2437362bc6f04c7cadc4d9051
Gri-ffin <gameshunter359@gmail.com>
2022-10-27 19:13:31 +0100

    add tooltip

--- src/components/About/SocialMedia.js
+++ src/components/About/SocialMedia.js
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Container, Heading, Link, useColorModeValue } from '@chakra-ui/react'
+import {
+  Container,
+  Heading,
+  Link,
+  Tooltip,
+  useColorModeValue
+} from '@chakra-ui/react'
 import { Section } from '../Section'
 import {
   IoLogoGithub,
@@ -17,68 +23,76 @@ export const ReachOut = () => {
           Where to find me
         </Heading>
         <Container display="flex" flexDirection="row">
-          <Link
-            href="https://www.linkedin.com/in/yassine-tadlaoui/"
-            color={useColorModeValue('gray.900', 'whiteAlpha.900')}
-            isExternal
-          >
-            <motion.button
-              whileHover={{
-                scale: 1.2,
-                transition: { duration: 0.3 }
-              }}
+          <Tooltip label="LinkedIn">
+            <Link
+              href="https://www.linkedin.com/in/yassine-tadlaoui/"
+              color={useColorModeValue('gray.900', 'whiteAlpha.900')}
+              isExternal
             >
-              <IoLogoLinkedin size={50} />
-            </motion.button>
-          </Link>
-          <Link
-            href="https://github.com/Gri-ffin"
-            color={useColorModeValue('gray.900', 'whiteAlpha.900')}
-            marginLeft="1.5rem"
-            isExternal
-          >
-            <motion.button
-              whileHover={{
-                scale: 1.2,
-                transition: { duration: 0.3 }
-              }}
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.3 }
+                }}
+              >
+                <IoLogoLinkedin size={40} />
+              </motion.button>
+            </Link>
+          </Tooltip>
+          <Tooltip label="Github">
+            <Link
+              href="https://github.com/Gri-ffin"
+              color={useColorModeValue('gray.900', 'whiteAlpha.900')}
+              marginLeft="1.5rem"
+              isExternal
             >
-              <IoLogoGithub size={50} />
-            </motion.button>
-          </Link>
-          <Link
-            key="Email"
-            href="mailto:yassinetadlaoui8@gmail.com"
-            color={useColorModeValue('gray.900', 'whiteAlpha.900')}
-            marginLeft="1.5rem"
-            isExternal
-          >
-            <motion.button
-              whileHover={{
-                scale: 1.2,
-                transition: { duration: 0.3 }
-              }}
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.3 }
+                }}
+              >
+                <IoLogoGithub size={40} />
+              </motion.button>
+            </Link>
+          </Tooltip>
+          <Tooltip label="Mail">
+            <Link
+              key="Email"
+              href="mailto:yassinetadlaoui8@gmail.com"
+              color={useColorModeValue('gray.900', 'whiteAlpha.900')}
+              marginLeft="1.5rem"
+              isExternal
             >
-              <IoMdMail size={50} />
-            </motion.button>
-          </Link>
-          <Link
-            key="Resume"
-            href="/resume.pdf"
-            color={useColorModeValue('gray.900', 'whiteAlpha.900')}
-            marginLeft="1.5rem"
-            isExternal
-            download
-          >
-            <motion.button
-              whileHover={{
-                scale: 1.2,
-                transition: { duration: 0.3 }
-              }}
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.3 }
+                }}
+              >
+                <IoMdMail size={40} />
+              </motion.button>
+            </Link>
+          </Tooltip>
+          <Tooltip label="Resume">
+            <Link
+              key="Resume"
+              href="/resume.pdf"
+              color={useColorModeValue('gray.900', 'whiteAlpha.900')}
+              marginLeft="1.5rem"
+              isExternal
+              download
             >
-              <IoMdDocument size={50} />
-            </motion.button>
-          </Link>
+              <motion.button
+                whileHover={{
+                  scale: 1.2,
+                  transition: { duration: 0.3 }
+                }}
+              >
+                <IoMdDocument size={40} />
+              </motion.button>
+            </Link>
+          </Tooltip>
         </Container>
       </Section>
     </Container>
