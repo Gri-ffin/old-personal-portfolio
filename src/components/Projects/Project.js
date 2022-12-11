75015769a504690f4dbfcaf8f5046fcb7ae4e93d
Gri-ffin <gameshunter359@gmail.com>
2022-12-11 23:49:42 +0100

    remove techs prop

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import { Container } from '@chakra-ui/react'
 import { Section } from '../Section'
-import { ProjectGridItem } from './Projects'
+import { ProjectGridItem } from './ProjectGridItem'
 
-const Project = ({ id, title, image, techs, children }) => (
+const Project = ({ id, title, image, children }) => (
   <Container>
     <Section>
-      <ProjectGridItem id={id} title={title} image={image} techs={techs}>
+      <ProjectGridItem id={id} title={title} image={image}>
         {children}
       </ProjectGridItem>
     </Section>
